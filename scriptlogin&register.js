4a5af24126d8dae89416d50194b1097b2228066d
happyfury006 <happyfury006@gmail.com>
2023-10-18 23:02:18 +0200

    test switch police

--- scriptlogin&register.js
+++ scriptlogin&register.js
@@ -1,8 +1,10 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const changeLanguage = document.querySelector('.LanguageSelector')
-const Police = document.querySelector('.PoliceAurebesh')
+const changeLanguage = document.querySelector('.LanguageSelector');
+const PoliceAurebesh = document.querySelector('.PoliceAurebesh');
+const PoliceAurebesh2 = document.querySelector('.PoliceAurebesh2');
+var choixPolice=0;
 
 registerLink.addEventListener('click',()=> {
     wrapper.classList.add('active');
@@ -11,5 +13,19 @@ loginLink.addEventListener('click',()=> {
     wrapper.classList.remove('active');
 });
 changeLanguage.addEventListener('click',()=> {
-    Police.classList.add('active');
+
+    choixPolice = choixPolice +1;
+    choixPolice=choixPolice%2;
+    switch (choixPolice) {
+        case 0:
+            PoliceAurebesh.classList.add('active');
+            PoliceAurebesh2.classList.remove('active');
+            break;
+        case 1:
+            PoliceAurebesh2.classList.add('active');
+            PoliceAurebesh.classList.remove('active');
+            break;
+        default:
+            break;
+    }
 });
